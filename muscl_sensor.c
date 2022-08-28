bd460b1c848301c1d51c18d723c5062938aa418e
lukaillich <60137197+lukaillich@users.noreply.github.com>
2022-08-28 17:12:04 +0200

    UART, muscle sensor

---
+++ muscl_sensor.c
@@ -0,0 +1,38 @@
+/*
+ * muscle_sensor.c
+ *
+ * Created: 17.08.2022. 16:25:33
+ * Author : ilkri
+ */ 
+
+#define F_CPU 7372800UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+
+void UART_init(long USART_BAUDRATE) {
+	UCSRB |= (1 << RXEN) | (1 << TXEN);	/* Turn on transmission and reception */
+	UCSRC |= (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1);/* Use 8-bit char size */
+	UBRRL = BAUD_PRESCALE;			/* Load lower 8-bits of the baud rate */
+	UBRRH = (BAUD_PRESCALE >> 8);		/* Load upper 8-bits*/
+}
+
+unsigned char UART_RxChar() {
+	while ((UCSRA & (1 << RXC)) == 0);/* Wait till data is received */
+	return(UDR);		/* Return the byte */
+}
+
+int main(void) {
+    UART_init(USART_BAUDRATE);
+    sei();
+	
+    while (1) {
+    }
+}
+
